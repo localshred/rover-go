dae2002989561fd6d20d06b5155a5b68541103dc
BJ Neilsen <bj.neilsen@gmail.com>
2019-04-25 14:50:13 -0400

    Initial rover timing middleware and associated functions

---
+++ rover.go
@@ -0,0 +1,78 @@
+package rover
+
+import (
+	"errors"
+	"fmt"
+	"github.com/DataDog/datadog-go/statsd"
+	"github.com/labstack/echo"
+)
+
+const (
+	statsdRequestKeyContextKey = "rover.statsd.requestKey"
+)
+
+type (
+	// Rover : Rover config
+	Rover struct {
+		StatsdClient *statsd.Client
+
+		// AddTagsForRequest is a callback you can provide to generate your own tags for each request to be
+		// sent with the timing metrics. The tags will be requested after the main request handler has
+		// completed its work.
+		AddTagsForRequest func(context echo.Context) []string
+
+		// GetRequestKey is a callback that returns the request key to be appended to the statsd client's
+		// Namespace (if any). If this callback is not provided, the request key will be extracted from the
+		// echo context if available. Otherwise, a request key will be auto-generated based on the Request URI.
+		//
+		// The preferred way of specifying the request key is to use the config.RequestKeyMiddleware function
+		// to statically set the rqeuest key on the echo context. See that function for more info.
+		//
+		// If the Namespace was "mycompany.myapp.prod" and GetRequestKey returns "get.list_users.timing", the full
+		// keyspace will be "mycompany.myapp.prod.get./users.timing".
+		GetRequestKey func(context echo.Context) string
+	}
+)
+
+// New : Creates a new rover configuration with the given statsd client and sensible defaults.
+func New(statsdClient *statsd.Client) *Rover {
+	return &Rover{
+		StatsdClient:  statsdClient,
+		GetRequestKey: defaultGetRequestKey,
+	}
+}
+
+// Tag : Builds a tag given a namespace and value and joining them with a colon.
+//
+//       Tag("foo:bar", "baz") -> "foo:bar:baz"
+//
+func Tag(namespace, value string) string {
+	return fmt.Sprintf("%s:%s", namespace, value)
+}
+
+func defaultGetRequestKey(context echo.Context) string {
+	if requestKey := context.Get(statsdRequestKeyContextKey); requestKey != nil {
+		return requestKey.(string)
+	}
+	return context.Request().URL.Path
+}
+
+func (rover *Rover) generateRequestKey(context echo.Context) string {
+	if rover.GetRequestKey == nil {
+		panic(errors.New("GetRequestKey callback cannot be nil"))
+	}
+	return rover.GetRequestKey(context)
+}
+
+func (rover *Rover) getTagsForRequest(context echo.Context) []string {
+	request := context.Request()
+	tags := []string{
+		Tag("http:host", request.Host),
+		Tag("http:method", request.Method),
+		Tag("http:remote-addr", request.RemoteAddr),
+	}
+	if rover.AddTagsForRequest != nil {
+		return append(tags, rover.AddTagsForRequest(context)...)
+	}
+	return tags
+}
---
+++ timing.go
@@ -0,0 +1,23 @@
+package rover
+
+import (
+	"github.com/labstack/echo"
+	"time"
+)
+
+// TimingMiddleware : An Echo middleware to add timing metrics around each Echo Request.
+func (rover *Rover) TimingMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(context echo.Context) (err error) {
+			requestStart := time.Now()
+			err = next(context)
+			duration := time.Since(requestStart)
+
+			requestKey := rover.generateRequestKey(context)
+			tags := rover.getTagsForRequest(context)
+			rover.StatsdClient.Timing(requestKey, duration, tags, 1)
+
+			return
+		}
+	}
+}
